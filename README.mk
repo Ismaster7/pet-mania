4cee70506ca48ab3d5301077fe148910c09ea3ad
Ismael C Lima <ismael.lima2000@outlook.com.br>
2023-12-07 18:30:27 -0300

    Commit inicial. Apenas adição dos arquivos essenciais do pŕojeto e do Readme

---
+++ README.mk
@@ -0,0 +1,7 @@
+### Adote um Pet Hoje! ###
+este projeto foi criado apenas para fins de teste de diversas propriedades web,
+todavia, tendo tempo, tornarei este site cada vez melhor ao ponto que possa, realemnte,
+entregar ele de graça para alguma ong ou petshop que tenha o intuito de auxiliar na
+adoção de animais.
+
+
